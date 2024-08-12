115e05942d5e7b69076aa8b3ad1727cbc22f05e1
MARCOS VICTOR DE ANDRADE RIBEIRO <20231148060017@ifrn.local>
2024-08-12 17:53:55 -0300

    subindo permissões

--- proj1/backend/src/routers/DatasetRouter.js
+++ proj1/backend/src/routers/DatasetRouter.js
@@ -2,10 +2,11 @@ import express from "express"
 import {upload} from "../middleware/uploadFileMiddleware.js"
 import {createDataset,getDatasets} from "../controller/DatasetController.js"
 import {verifyToken} from "../middleware/JwtMiddleware.js"
+import checkPermission from "../middleware/PermissionMiddleware.js"
 
 const router = express.Router()
 
-router.post("/",verifyToken ,upload.single("file"),createDataset);
+router.post("/",verifyToken ,checkPermission("CREATE_DATASET"),upload.single("file"),createDataset);
 router.get("/",verifyToken,getDatasets)
 
 export default router;
